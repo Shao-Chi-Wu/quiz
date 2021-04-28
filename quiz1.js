afbfa8b50397f4600de8a19d4f7280c0d82cc97a
Shao-Chi-Wu <roywu0314@gmail.com>
2021-04-28 22:43:58 +0800

    quiz1.3

--- quiz1.js
+++ quiz1.js
@@ -1,12 +1,18 @@
-  
 function change() {
-    $("#hid123").html("CSIE@CGU");
-    $("#pid123").html("怎麼那麼棒")
+    $(document).ready(function() {
+        $("button").click(function() {
+            $("#He").text("CSIE@CGU");
+        });
+
+        $("button").click(function() {
+            $("#Par").text("怎麼那麼棒！！.");
+        });
+    });
 }
 
-function new_button() {
-    var btn="<button id='change'>Change this document</button>";
-    $("body").append(btn);
-    $("#change").click(change);
+function myFunction() {
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "Change this document.";
+    document.body.appendChild(btn);
+    btn.addEventListener("click", change);
 }
-$("#btn").click(new_button);
